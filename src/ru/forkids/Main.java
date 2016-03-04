d9ea32be1007de04b563558dab97bde601b5f6bb
ScriptAv <avderuno@gmail.com>
2016-03-04 18:57:22 +0300

    qwe

---
+++ src/ru/forkids/Main.java
@@ -0,0 +1,11 @@
+package ru.forkids;
+
+/**
+ * Created by script on 04.03.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("edrverv");
+        Main m = new Main;
+    }
+}
