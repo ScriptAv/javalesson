509f53d1a742095774dae01a1334cb4161334ef4
ScriptAv <avderuno@gmail.com>
2016-03-04 17:50:16 +0300

    qwe

--- src/MainArr3.java
+++ src/MainArr3.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
+
 /**
  * Created by script on 04.03.16.
  */
 public class MainArr3 {
     public static void main(String[] args) {
-        System.out.println("detrgb");
+        int[][] cells;
+        cells = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(cells[j][i]);
+            }
+            System.out.println();
+        }
     }
 }
